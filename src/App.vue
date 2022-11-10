d732bd7a3888dc28523ad982597bb143e1be9973
BiagioButta <biagiob940@gmail.com>
2022-11-10 16:35:42 +0100

    Creazione e collegamento dei componenti

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-
+    <HeaderComponents/>
+  </div>
+  <div>
+    <MainContents/>
+  </div>
+  <div>
+    <AzureComponents/>
+  </div>
+  <div>
+    <FooterComponents/>
   </div>
 </template>
 
 <script>
+
+  import HeaderComponents from './components/HeaderComponents.vue';
+  import MainContents from './components/MainContents.vue';
+  import AzureComponents from './components/AzureComponents.vue';
+  import FooterComponents from './components/FooterComponents.vue';
+
   export default {
-    
+    components: {
+      HeaderComponents,
+      MainContents,
+      AzureComponents,
+      FooterComponents
+    }
   }
 </script>
 
